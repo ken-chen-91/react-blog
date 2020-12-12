d9c661e6be3e99d102ad96f31ed0bd8eb58cad35
kenchen <a330120231@gmail.com>
2020-12-12 00:41:39 -0800

    完成登录验证

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI:"mongodb+srv://KenChen:a5711860@reacgt-blog.mb4oi.mongodb.net/<dbname>?retryWrites=true&w=majority"
+}
--- index.js
+++ index.js
@@ -4,15 +4,13 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 
-const config = require('./config/key')
+const config = require("./config/key");
 
 const { User } = require("./models/user.js");
+const { auth } = require("./middleware/auth");
 
 mongoose
-  .connect(
-    config.mongoURI,
-    { useNewUrlParser: true }
-  )
+  .connect(config.mongoURI, { useNewUrlParser: true })
   .then(() => {
     console.log("DB connected");
   })
@@ -24,19 +22,57 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.json({"hello ~":"Hi ~~"})
-})
+app.get("/", (req, res) => {
+  res.json({ "hello ~": "Hi ~~" });
+});
+
+app.get("/api/user/auth", auth, (req, res) => {
+  res.status(200).json({
+    _id: req._id,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    token:req.user.token
+  });
+});
 
 app.post("/api/users/register", (req, res) => {
   const user = new User(req.body);
 
-  user.save((err, userData) => {
+  user.save((err, doc) => {
     if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+      userData: doc,
+    });
   });
+});
 
-  return res.status(200).json({
-    success: true,
+app.post("/api/user/login", (req, res) => {
+  // find the email
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if (!user)
+      return res.json({
+        loginSuccess: false,
+        message: "Auth failed, email not found",
+      });
+    
+    // comparePassword
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch) {
+        return res.json({ loginSuccess: false, message: "Wrong password" });
+      }
+    });
+
+    // generateToken
+    user.generateToken((err, user) => {
+      if (err) return res.status(400).send(err);
+      res.cookie("x_auth", user.token).status(200).json({
+        loginSuccess: true,
+      });
+    });
   });
 });
 
